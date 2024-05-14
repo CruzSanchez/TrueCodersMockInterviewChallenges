85bd939618583f6771a78d3ca8b632a69478cf08
Cruz Sanchez <cruzsanchez9209@gmail.com>
2024-05-14 17:43:15 -0500

    Add project files.

---
+++ TrueCodersMockInterviewChallenges.Library/Challenge.cs
@@ -0,0 +1,35 @@
+﻿namespace TrueCodersMockInterviewChallenges.Library
+{
+    public static class Challenge
+    {
+        //Challenge 1
+        //Write a method called GenerateSequence that takes an int named n as parameters and returns an int array containing the numbers from 1 to n.
+        //Example - int n = 5 your return value would be an array that looks like this [1, 2, 3, 4, 5]
+        //Always assume n >= 0 if the input is 0, return an empty array
+
+
+        //Challenge 2
+        //Write a method named CountVowels that takes a string as a parameter and returns an int representing the number of vowels found within the string.
+        //Vowels are considered to be (aeiou) NOT 'y', ignore case - 'A' and 'a' are both valid inputs
+        //If there are not any vowels in the input string return 0
+
+
+        //Challenge 3
+        //Create a method named FindLargest that accepts an int array as parameters and returns an int representing the largest number.
+        //Example - [3, 1, 10, 7, 6] you would return 10 since it is the largest number in the array
+        //If the input array is empty return 0
+
+
+        //Challenge 4
+        //Write a method named FindOnesAndZeros that accepts an int array as a parameter and returns an int representing the count of the number of 1s AND 0s present in the array.
+        //Any other value can be ignored.
+        //Example - input [1, 1, 0, 3, 0, 0, 2, 1, 2, 3, 5] you would return 6 since there are 3 ones and 3 zeros
+        //If the input array is empty return 0
+
+
+        //Challenge 5        
+        //Write a method that takes a bool array as parameters and returns an int that represents the number of times false appears
+        //Example - [true, false, false] would return 2 since there are 2 false elements in the array
+        //If the input array is empty return 0
+    }
+}
